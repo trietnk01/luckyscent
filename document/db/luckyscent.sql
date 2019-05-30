bfdc1aa5f246b84260a17aa1a4cd099f51d9bb91
dienkim <trietnk01@gmail.com>
2019-05-30 12:09:15 +0700

    cập nhật

--- document/db/luckyscent.sql
+++ document/db/luckyscent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2019 at 07:06 AM
+-- Generation Time: May 30, 2019 at 07:08 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.1.26
 
